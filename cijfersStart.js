dd9374f9a4597db1793a25627bd25b40b1920dc4
LeroyAndrade <30168@ma-web.nl>
2020-04-08 12:56:40 +0200

    Opdracht 1 tot en met 4 met aanpassing code
    Opdracht 1 tot en met 4 met aanpassing code in de vorm van een Math.floor(MAth.random()) toegevoegd aan de opdracht.

--- cijfersStart.js
+++ cijfersStart.js
@@ -16,15 +16,17 @@ function cijferObj(opdracht, cijfer) {
 let cijferOb = this;
 cijferOb.Opdracht = opdracht;
 cijferOb.Cijfer = cijfer;
-
-if (cijfer <= 10 && cijfer > 0)
-{
-  cijferOb.Cijfer = cijfer; //rand()
-} else {
-cijferOb.Cijfer =5;
-}
-}
+for(i=0; i< 11; i++){
+  if (cijfer !== i)
+  {
+  cijferOb.Cijfer += "Geen waarde ingevuld, u krijgt een willekeurig getal ", cijferOb.Cijfer =Math.floor(Math.random()*10)+1;
+  } else {
+          cijferOb.Cijfer = cijfer;
+         }
+  }
+  }
 let cijfer1 = new cijferObj("OBJ");
+//let cijfer1 = new cijferObj("OBJ",10);
 document.write(cijfer1.Cijfer);
 
 // OPDRACHT 2.
@@ -34,29 +36,42 @@ document.write(cijfer1.Cijfer);
         // Totaal dus 9 opdrachten
         // Maak voor al deze opdrachten een cijfer variabele aan
 // Kijk in week6 hoe je een variabele aanmaakt met het keyword new
-    let obj0 = new Opdracht('FRO',  'Opdracht1.0');
-    let obj1 = new Opdracht('FRO',  'Opdracht1.1');
-    let obj2 = new Opdracht('PR',   'Opdracht1.2');
-    let obj3 = new Opdracht('SLB',  'Opdracht1.3');
-    let obj4 = new Opdracht('BAP',  'Opdracht1.4');
-    let obj5 = new Opdracht('NED',  'Opdracht1.5');
-    let obj6 = new Opdracht('CMS',  'Opdracht1.6');
-    let obj7 = new Opdracht('K-ID', 'Opdracht1.7');
-    let obj8 = new Opdracht('ENG',  'Opdracht1.8');
-    let obj9 = new Opdracht('PRO',  'Opdracht1.9');
+    let op0 = new Opdracht('FRO',  'Opdracht1.0');
+    let op1 = new Opdracht('FRO',  'Opdracht1.1');
+    let op2 = new Opdracht('PR',   'Opdracht1.2');
+    let op3 = new Opdracht('SLB',  'Opdracht1.3');
+    let op4 = new Opdracht('BAP',  'Opdracht1.4');
+    let op5 = new Opdracht('NED',  'Opdracht1.5');
+    let op6 = new Opdracht('CMS',  'Opdracht1.6');
+    let op7 = new Opdracht('K-ID', 'Opdracht1.7');
+    let op8 = new Opdracht('ENG',  'Opdracht1.8');
+    let op9 = new Opdracht('PRO',  'Opdracht1.9');
 
-/*
-let c0 = new cijferObj(obj0.beschrijving,obj0.vak);
-let c1 = new cijferObj(obj1.beschrijving,obj1.vak);
-let c2 = new cijferObj(obj2.beschrijving,obj2.vak);
-let c3 = new cijferObj(obj3.beschrijving,obj3.vak);
-let c4 = new cijferObj(obj4.beschrijving,obj4.vak);
-let c5 = new cijferObj(obj5.beschrijving,obj5.vak);
-let c6 = new cijferObj(obj6.beschrijving,obj6.vak);
-let c7 = new cijferObj(obj7.beschrijving,obj7.vak);
-let c8 = new cijferObj(obj8.beschrijving,obj8.vak);
-let c9 = new cijferObj(obj9.beschrijving,obj9.vak);
-*/
+//Manier 0
+let c0 = new cijferObj(op0.beschrijving,op0.vak);
+let c1 = new cijferObj(op1.beschrijving,op1.vak);
+let c2 = new cijferObj(op2.beschrijving,op2.vak);
+let c3 = new cijferObj(op3.beschrijving,op3.vak);
+let c4 = new cijferObj(op4.beschrijving,op4.vak);
+let c5 = new cijferObj(op5.beschrijving,op5.vak);
+let c6 = new cijferObj(op6.beschrijving,op6.vak);
+let c7 = new cijferObj(op7.beschrijving,op7.vak);
+let c8 = new cijferObj(op8.beschrijving,op8.vak);
+let c9 = new cijferObj(op9.beschrijving,op9.vak);
+
+//Manier 1
+let c00 = new cijferObj(op0.beschrijving,op0.vak);
+let c11 = new cijferObj(op1.beschrijving,op1.vak);
+let c22 = new cijferObj(op2.beschrijving,op2.vak);
+let c33 = new cijferObj(op3.beschrijving,op3.vak);
+let c44 = new cijferObj(op4.beschrijving,op4.vak);
+let c55 = new cijferObj(op5.beschrijving,op5.vak);
+let c66 = new cijferObj(op6.beschrijving,op6.vak);
+let c77 = new cijferObj(op7.beschrijving,op7.vak);
+let c88 = new cijferObj(op8.beschrijving,op8.vak);
+let c99 = new cijferObj(op9.beschrijving,op9.vak);
+
+//let cijferRand=
 
 // OPDRACHT 3.
 // Vul de arrays met de opdrachten en cijfers die je hebt aangemaakt.
